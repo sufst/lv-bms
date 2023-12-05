f467ae17d69acf416cd06096e1667dee4f6ebc53
Scaniox <alexl.mills3@gmail.com>
2023-12-05 19:21:25 +0000

    inital copy from can interface demo code

---
+++ lv-bms-firmware.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=lv-bms-firmware.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/lv-bms-firmware.X.production.hex
