51370b98a19de3c8e281daa92714cf2cb9c2053d
Alex Mills <alexl.mills3@gmail.com>
2025-02-11 21:08:16 +0000

    indicator lights done, now testing it

---
+++ lv-bms-firmware.X/tests/indicator_test_main.c
@@ -0,0 +1,17 @@
+/*
+ * File:   indicator_test_main.c
+ * Author: alexl
+ *
+ * Created on February 11, 2025, 8:51 PM
+ */
+
+
+#include <xc.h>
+#include "../indicator_lights.h"
+
+void main(void) {
+    return;
+    
+    
+    
+}
